9a5af133316ace89ab0ad52ce12e56a808b3831a
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-27 13:43:19 +1100

    create Movie component

---
+++ src/components/Movie.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Movie = props => {
+  const { title, id, image } = props;
+  return (
+    <section>
+      <figure>
+        <img src={image} alt={title} />
+      </figure>
+      <h3>{title}</h3>
+      <Button id={id} />
+    </section>
+  );
+};
+
+Movie.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequireds
+};
+
+export default Movie;
