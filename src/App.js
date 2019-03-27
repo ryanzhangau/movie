13f8628c03a56c9f1c134a3e1a2ed6481ac28ab4
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-27 11:58:27 +1100

    add header to main app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
