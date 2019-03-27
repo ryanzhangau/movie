2581831712c495ca9c0c86e88f6710f963b10c00
Ryan Zhang <ryanzhangau@gmail.com>
2019-03-27 11:39:05 +1100

    create header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">MF</div>
+      <div className="site-title">Movie Finder</div>
+    </header>
+  );
+};
+
+export default Header;
